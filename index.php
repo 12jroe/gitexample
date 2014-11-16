46a357b5558aa9c2bb5e599f77b9808939273b37
Joe Conradt <joeconradt@wi.rr.com>
2014-11-16 00:16:31 -0600

    added a useless if statement

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 echo "Hello, John<br>";
 
-echo "How are you today John?";
+echo "How are you today John?\n";
+
+$name = "John";
+
+if(substr($name, -1) == "n") {
+	echo "John's name ends in 'n'";
+}
 ?>
