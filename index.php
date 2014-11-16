d4fecdfb6c6dbc62f8d2c1df57c243f321291650
Jack Roe <rattles7@hotmail.com>
2014-11-16 00:20:51 -0600

    Made john all caps and added a conditional

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 echo "Hello, John<br>";
 
-echo "How are you today John?";
+echo "How are you today John?<br>";
+
+$name = "JOHN";
+
+if ($name == "John") {
+	echo $name . " is normal today";
+} else if ($name == "JOHN") {
+	echo $name . "is all CAPS today";
+}
 ?>
